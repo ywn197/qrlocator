09003676e8d1cc9709f3ba10a8f00635fe497f06
yuki <yuki@example.com>
2025-08-27 01:25:22 +0900

    fix a bug

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ class QRReader {
 
     async init() {
         try {
-            this.stream = await navigator.mediaDevices.getUserMedia({ video: {width:1920 height:1080 facingMode: { exact: "environment" }}, audio: false });
+            this.stream = await navigator.mediaDevices.getUserMedia({
+                video: {width:1920, height:1080, facingMode: { exact: "environment" }}, 
+                audio: false });
             this.videoEl.srcObject = this.stream;
             this.videoEl.play();
             this.videoEl.onloadeddata = () => {
